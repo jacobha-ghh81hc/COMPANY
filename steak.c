c46c3a4293eac5a7319bc8a5c14b2726292dfcf6
jacobha-ghh81hc <huygiap601@gmail.com>
2020-08-22 22:37:11 +0700

    Testing the cmath

--- steak.c
+++ steak.c
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main () {
-	cout << "This is jacobha" << endl;
+
+	    // typical usage
+    std::cout << "sin(pi/6) = " << std::sin(pi/6) << '\n'
+              << "sin(pi/2) = " << std::sin(pi/2) << '\n'
+              << "sin(-3*pi/4) = " << std::sin(-3*pi/4) << '\n';
 	return 0;
 }
 
